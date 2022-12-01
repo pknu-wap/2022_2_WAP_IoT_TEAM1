665f013ee4c7a652c6a96af9b28625be2c596f02
Lee_In_Jae <lhkgg7548@gmail.com>
2022-12-01 23:53:00 +0900

    time 구현

--- test1026/app/src/main/java/com/example/test1026/TimeActivity.kt
+++ test1026/app/src/main/java/com/example/test1026/TimeActivity.kt
@@ -1,9 +1,17 @@
 package com.example.test1026
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
+import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.TimePicker
+import androidx.appcompat.app.AppCompatActivity
+import com.example.test1026.databinding.ActivityTimeBinding
+import kotlinx.android.synthetic.main.activity_time.*
+import java.util.Calendar
+import java.util.Calendar.getInstance
+import java.util.logging.Logger.global
 
 class TimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,15 +19,38 @@ class TimeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_time)
         //객체 설정
         val timePicker: TimePicker = findViewById(R.id.timePicker)
-        val timeText: TextView = findViewById(R.id.time_text)
-
+        setTimePickerInterval(timePicker)
         //TimePicker 값 변경 이벤트
         timePicker.setOnTimeChangedListener { timePicker, hourOfDay, minute ->
-            //변경 된 값 Textview에 담기
-            timeText.text = "${hourOfDay}시 ${minute}분"
-        }
-    }//onCreate
+            timeText.text="${hourOfDay}시 ${minute*5}분"
 
-        
-        
+        }//setOnTimeChangedListener
+
+
+    }//onCreate
+}//AppCompatActivity
+
+// 시간 간격을 5분 단위로 바꿔주는 함수 setTimePickerInterval()
+private fun setTimePickerInterval(timePicker: TimePicker) {
+    lateinit var binding: ActivityTimeBinding
+    try {
+        val TIME_PICKER_INTERVAL = 5
+        val minutePicker = timePicker.findViewById(
+            Resources.getSystem().getIdentifier(
+                "minute", "id", "android"
+            )
+        ) as NumberPicker
+        minutePicker.minValue = 0
+        minutePicker.maxValue = 60 / TIME_PICKER_INTERVAL - 1
+        val displayedValues: MutableList<String> = ArrayList()
+        var i = 0
+        while (i < 60) {
+            displayedValues.add(String.format("%02d", i))
+            i += TIME_PICKER_INTERVAL
+        }
+        minutePicker.displayedValues = displayedValues.toTypedArray()
+
+    } //try
+    catch (e: Exception) {
     }
+} //setTimePickerInterval()
